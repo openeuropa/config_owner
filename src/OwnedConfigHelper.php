5304787d3b7f30dca24feabbc73f3d2f0f2baa02
upchuk <danny@webomelette.com>
2018-11-26 17:47:16 +0100

    OPENEUROPA-1318: Fixing the third party settings removal.

--- src/OwnedConfigHelper.php
+++ src/OwnedConfigHelper.php
@@ -91,9 +91,63 @@ class OwnedConfigHelper {
    *
    * @return array
    *   The resulting array.
+   *
+   * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+   * @SuppressWarnings(PHPMD.NPathComplexity)
    */
   public static function removeThirdPartySettings(array $config, array $ignored_keys = []) {
     $flat = static::flattenConfig($config);
+
+    // First we need to determine the locations of all the third party settings
+    // in the config (their parents).
+    $third_party_settings_parents = [];
+    foreach (array_keys($flat) as $key) {
+      if (strpos($key, 'third_party_settings.') === FALSE) {
+        continue;
+      }
+
+      $parts = explode('.', $key);
+      $key = array_search('third_party_settings', $parts);
+      $parents = array_slice($parts, 0, $key + 1, TRUE);
+      $third_party_settings_parents[] = implode('.', $parents);
+    }
+    $third_party_settings_parents = array_unique($third_party_settings_parents);
+
+    // Next, we need to identify the third party settings we want to remove
+    // straight from the parents level (i.e those which do not have children
+    // that are ignored - owned).
+    foreach ($third_party_settings_parents as $parent) {
+      $found = FALSE;
+      foreach ($ignored_keys as $key) {
+        if (strpos($key, $parent) === 0) {
+          $found = TRUE;
+        }
+      }
+
+      if ($found) {
+        // If this is a parent of one of the ignored keys, we remove it from
+        // the array so that it won't get unset later.
+        $parent_key = array_search($parent, $third_party_settings_parents);
+        unset($third_party_settings_parents[$parent_key]);
+      }
+    }
+
+    // After determining the final list of parents that can be fully removed,
+    // we need to remove them completely.
+    foreach (array_keys($flat) as $key) {
+      foreach ($third_party_settings_parents as $parent) {
+        if (strpos($key, $parent) === 0) {
+          $parts = explode('.', $parent);
+          NestedArray::unsetValue($config, $parts);
+          // Remove it also from the flattened array so we don't have it in the
+          // next step below.
+          unset($flat[$key]);
+        }
+      }
+    }
+
+    // Lastly, we loop through the flatten keys and remove the third party
+    // settings that are not ignored (i.e owned).
     foreach (array_keys($flat) as $key) {
       if (strpos($key, 'third_party_settings.') === FALSE) {
         continue;
@@ -104,8 +158,6 @@ class OwnedConfigHelper {
       }
 
       $parents = explode('.', $key);
-      $third_party_key = array_search('third_party_settings', $parents);
-      $parents = array_slice($parents, 0, $third_party_key + 1, TRUE);
       NestedArray::unsetValue($config, $parents);
     }
 
